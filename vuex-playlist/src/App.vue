c18dbdfb906f36c5136302d84fdf4b7b0908911c
Julian Tanausu Villar <juliantanausuvillar@MacBook-Pro-de-Julian.local>
2018-06-04 22:10:28 +0200

    demos vueX

---
+++ vuex-playlist/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <!-- v-bind:products="products" asim odifica el products de data -->
+    <product-list-one></product-list-one>
+    <product-list-two></product-list-two>
+  </div>
+</template>
+
+<script>
+import ProductListOne from './components/ProductListOne.vue';
+import ProductListTwo from './components/ProductListTwo.vue';
+
+export default {
+  components:{
+    'product-list-one': ProductListOne,
+    'product-list-two': ProductListTwo
+  },
+  name: 'app',
+  data () {
+    return {
+      /*
+      products:[
+        {name:'Banana Skin', price: 20},
+        {name:'Shiny Star', price: 40},
+        {name:'Green Shells', price: 60},
+        {name:'Red Shells', price: 80}
+      ]
+      */
+    }
+  }
+}
+</script>
+
+<style>
+
+body{
+  font-family: Ubuntu;
+  color: #555;
+}
+</style>
---
+++ vuex-playlist/src/components/ProductlistOne.vue
@@ -0,0 +1,81 @@
+<template>
+  <div id="product-list-one">
+    <h2>Product List One</h2>
+    <ul>
+      <li v-for="product in saleProducts">
+        <span class="name">{{ product.name }}</span>
+        <span class="price">{{ product.price }}€</span>
+      </li>
+    </ul>
+    <button v-on:click="reducePrice(4)">Reduce Price</button>
+  </div>
+</template>
+
+<script>
+import {mapActions} from 'vuex';
+import {mapGetters} from 'vuex';
+
+export default{
+/*llama a la var product de app o algo
+  props: ['products'],
+  */
+  computed: {
+    products(){
+      return this.$store.state.products
+    },
+
+    ...mapGetters([
+      'saleProducts'//,
+      //'siguiente'
+    ])
+
+
+  },
+  methods:{
+    /*
+    reducePrice: function(amount){
+      this.$store.state.products.forEach(product => {
+        product.price -= 1;
+      })
+      this.$store.dispatch('reducePrice', amount);//para llamar al action
+    }
+    */
+
+    ...mapActions([
+      'reducePrice' //action
+    ])
+  }
+
+
+}
+</script>
+
+<style scoped>
+
+#product-list-one{
+  background: #FFF8B1;
+  box-shadow: 1px 2px 3px rgba(0,0,0,0,2);
+  margin-bottom: 30px;
+  padding: 10px 20px;
+}
+
+#product-list-one ul{
+  padding: 0;
+  list-style-type: none;
+}
+
+#product-list-one li{
+  margin-right: 10px;
+  margin-top: 10px;
+  padding: 20px;
+  background: rgba(255,255,255,0.7);
+}
+
+.price{
+  font-weight: bold;
+  color:#E8800C;
+
+}
+
+
+</style>
